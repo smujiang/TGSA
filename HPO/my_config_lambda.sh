ec1a8e64e47822f1ff663efe47b3438ee87f2d2a
Jun Jiang <smujiang@gmail.com>
2023-09-04 12:05:33 -0500

    update HPO

---
+++ HPO/my_config_lambda.sh
@@ -0,0 +1,5 @@
+source_cfg -v cfg-my-settings_lambda.sh
+
+export CANDLE_MODEL_TYPE="SINGULARITY"
+export MODEL_NAME=/homes/ac.jjiang/project/Singularity/images/TGSA.sif
+export PARAM_SET_FILE=/home/ac.jjiang/project/TGSA/HPO/params.json
---
+++ HPO/my_config_polaris.sh
@@ -0,0 +1,6 @@
+source_cfg -v cfg-my-settings_polaris.sh
+
+
+export CANDLE_MODEL_TYPE="SINGULARITY"
+export MODEL_NAME=/lus/grand/projects/mayopath/Jun/HPO/Experiments/images/TGSA.sif
+export PARAM_SET_FILE=/lus/grand/projects/mayopath/Jun/HPO/Experiments/params.json
